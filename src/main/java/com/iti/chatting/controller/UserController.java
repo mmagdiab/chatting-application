b467fd12812f28c7dfd01435f9f01f9b9aa11369
Mohamed Magdy <14momagdi@gmail.com>
2022-10-11 23:40:01 +0200

    initial commit for User feat.
    add basic structure for the User.

---
+++ src/main/java/com/iti/chatting/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.iti.chatting.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    // TODO: Add user
+    // TODO: Update user data
+}

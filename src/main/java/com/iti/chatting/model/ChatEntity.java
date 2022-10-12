3bb5d02d13dd485d3d6c4c137651c2c409363e00
MoZyadaCS <mostafazyada@protonmail.com>
2022-10-13 01:52:27 +0200

    adding chat entity class

---
+++ src/main/java/com/iti/chatting/model/ChatEntity.java
@@ -0,0 +1,28 @@
+package com.iti.chatting.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "chat")
+public class ChatEntity {
+
+    @Id
+    @Column(name = "chat_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "chat_topic",length = 32)
+    private String topic;
+    @Column(name = "chat_password",length = 32)
+    private String password;
+
+    @ManyToMany
+    @JoinTable(name = "user_chat", joinColumns = {@JoinColumn(name = "user_chat_chat_id")},
+    inverseJoinColumns = {@JoinColumn(name = "user_chat_user_id")})
+    List<UserEntity> user;
+
+}

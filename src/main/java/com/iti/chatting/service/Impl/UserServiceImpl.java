792a2d5e2278ebab38dc1a50be143ef4846a3c30
Mohamed Magdy <14momagdi@gmail.com>
2022-10-12 20:17:51 +0200

    implement multiple methods
    Add Implementation for user entity, service and controller.

--- src/main/java/com/iti/chatting/service/Impl/UserServiceImpl.java
+++ src/main/java/com/iti/chatting/service/Impl/UserServiceImpl.java
@@ -1,4 +1,34 @@
 package com.iti.chatting.service.Impl;
 
-public class UserServiceImpl {
+import com.iti.chatting.model.UserEntity;
+import com.iti.chatting.repository.UserRepository;
+import com.iti.chatting.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserEntity addUser(UserEntity userEntity) {
+        userRepository.save(userEntity);
+        return userEntity;
+    }
+
+    @Override
+    public UserEntity updateUser(UserEntity userEntity) {
+        if (!userRepository.existsById(userEntity.getId())) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not found!");
+        }
+        userRepository.save(userEntity);
+        return userEntity;
+    }
 }
--- src/main/java/com/iti/chatting/service/UserService.java
+++ src/main/java/com/iti/chatting/service/UserService.java
@@ -1,9 +1,10 @@
 package com.iti.chatting.service;
 
+import com.iti.chatting.model.UserEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-    // TODO: Add user
-    // TODO: Update user data
+    public UserEntity addUser(UserEntity userEntity);
+    public UserEntity updateUser(UserEntity userEntity);
 }

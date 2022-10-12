d75d2a30eee9cb5e562943e732afafc103ce1637
MoZyadaCS <mostafazyada@protonmail.com>
2022-10-13 01:53:27 +0200

    adding chat repo class

---
+++ src/main/java/com/iti/chatting/repository/ChatRepository.java
@@ -0,0 +1,9 @@
+package com.iti.chatting.repository;
+
+import com.iti.chatting.model.ChatEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRepository extends JpaRepository<ChatEntity,Long> {
+}

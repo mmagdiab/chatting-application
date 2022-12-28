c6f0b06973165251ebed7fe31f801eb27f0b889d
Mohamed Magdy <14momagdi@gmail.com>
2022-12-28 13:55:26 +0200

    add sender name along with the message

--- src/main/java/com/iti/chatting/controller/SendMessageController.java
+++ src/main/java/com/iti/chatting/controller/SendMessageController.java
@@ -1,11 +1,14 @@
 package com.iti.chatting.controller;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import java.security.Principal;
 
 import static com.iti.chatting.ChattingApplication.MAIN_EXCHANGE;
 
@@ -19,10 +22,12 @@ public class SendMessageController {
 
     @PostMapping("/send")
     public String sendMessage(@RequestParam String message,
-                              @RequestParam String chatId) {
+                              @RequestParam String chatId,
+                              Authentication authentication) {
+        String name = authentication.getName();
         String routingKey = chatId;
         rabbitTemplate.convertAndSend(MAIN_EXCHANGE,
                 routingKey, message);
-        return "Message Sent Successfully";
+        return "Message Sent Successfully from " + name;
     }
 }
